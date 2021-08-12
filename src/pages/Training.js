368c6d0a1f2f70e3dae76fb84d35e6f55b35c3af
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-08-12 12:11:28 +0200

    add styling of form

--- src/pages/Training.js
+++ src/pages/Training.js
@@ -2,10 +2,31 @@ import "./Training.css";
 export default function Training() {
   return (
     <div className="training__exercise-form">
-      <form>
-        <label htmlFor="exercise">exercise</label>
-        <input type="text" name="exercise" required />
-      </form>
+      {/* <form> */}
+      {/* <label htmlFor="exercise">exercise</label> */}
+      <input
+        className="training__exercise-input"
+        type="text"
+        name="exercise"
+        required
+      />
+      {/* <label htmlFor="weight">weight</label> */}
+      <input
+        className="training__weight-input"
+        type="number"
+        name="weight"
+        required
+      />
+      <button className="training__weight-button">+</button>
+      {/* <label htmlFor="repetitions">repetitions</label> */}
+      <input
+        className="training__repetitions-input"
+        type="number"
+        name="repetitions"
+        required
+      />
+      <button className="training__repititions-button">+</button>
+      {/* </form> */}
     </div>
   );
 }
