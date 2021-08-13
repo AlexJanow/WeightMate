b1a16ff1958450c2809f2733c6efd94b2ffab906
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-08-13 14:57:24 +0200

    add regex for single exercise

--- src/pages/SingleExercise.js
+++ src/pages/SingleExercise.js
@@ -1,20 +1,36 @@
 import React from "react";
 import { useParams } from "react-router";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function SingleExercise() {
+  const [info, setInfo] = useState("");
+  const [img, setImg] = useState("");
   const { exerciseId } = useParams();
   useEffect(() => {
     const url = `https://wger.de/api/v2/exercise/${exerciseId}`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        setInfo(data);
+      });
   }, [exerciseId]);
 
+  //   useEffect(() => {
+  //     const imgUrl = `https://wger.de/api/v2/exerciseimage/${info.id}/thumbnails/?format=json`;
+  //     fetch(imgUrl)
+  //       .then((res) => res.json())
+  //       .then((data) => setImg(data.thumbnail.url));
+  //   }, [info]);
+
+  //   const descr = info.description;
+  //   const descrResult = info.replace(/(<([^>]+)>)/gi, "");
   return (
     <div>
-      test
-      {exerciseId}
+      {info.name}
+      {info.id}
+      {info && info.description.replace(/(<([^>]+)>)/gi, "")}
+      {/* <img src={`https://wger.de${img}`} /> */}
     </div>
   );
 }
