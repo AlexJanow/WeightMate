67139e9f20e5b2344e3d52cea01ec5be0d83696e
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-08-17 13:15:38 +0200

    add first accordion

--- src/pages/SingleExercise.js
+++ src/pages/SingleExercise.js
@@ -2,8 +2,26 @@ import React from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { Accordion } from "@material-ui/core";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 import "./SingleExercise.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "80%",
+  },
+  heading: {
+    width: "100%",
+    margin: "auto",
+  },
+}));
 export default function SingleExercise() {
+  const classes = useStyles();
   const [exerciseData, setExerciseData] = useState("");
 
   const { exerciseId } = useParams();
@@ -30,8 +48,27 @@ export default function SingleExercise() {
           src={exerciseData?.images ? exerciseData?.images[1]?.image : null}
         />
       </div>
-      <div className="singleExercise__description">
-        {exerciseData && exerciseData.description.replace(/(<([^>]+)>)/gi, "")}
+      <div className="singleExercise__accordion">
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <Accordion className={classes.root} justify="center">
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              {" "}
+              <Typography className={classes.heading}>Description</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                <div className="singleExercise__description">
+                  {exerciseData &&
+                    exerciseData.description.replace(/(<([^>]+)>)/gi, "")}
+                </div>
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Box>
       </div>
       <div className="singleExercise__variations">
         Variations:
