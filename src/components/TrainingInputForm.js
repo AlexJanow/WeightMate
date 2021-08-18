8cc36b57f2e7fb555d97d26252d852b6a24ffce2
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-08-18 12:43:45 +0200

    add form save to state as object

--- src/components/TrainingInputForm.js
+++ src/components/TrainingInputForm.js
@@ -1,18 +1,43 @@
 import React from "react";
 import "./TrainingInputForm.css";
+import { useState } from "react";
+import { useParams } from "react-router";
 export default function TrainingInputForm() {
+  const { exerciseId } = useParams();
+
+  const [trainingData, setTrainingData] = useState({
+    Id: exerciseId,
+    weight: "",
+    repetitions: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(trainingData);
+  };
+
   return (
-    <div className="trainingInputForm__wrapper">
+    <form onSubmit={handleSubmit} className="trainingInputForm__wrapper">
       <input
+        onChange={(e) =>
+          setTrainingData({ ...trainingData, weight: e.target.value })
+        }
+        value={trainingData.weight}
         className="training__weight-input"
         type="number"
         name="weight"
         placeholder="weight in kg"
         required
       />
-      <button className="training__weight-button">+</button>
+
+      {/* //Maybe only one button is needed
+       <button className="training__weight-button">+</button> */}
 
       <input
+        onChange={(e) =>
+          setTrainingData({ ...trainingData, repetitions: e.target.value })
+        }
+        value={trainingData.repetitions}
         className="training__repetitions-input"
         type="number"
         name="repetitions"
@@ -20,6 +45,6 @@ export default function TrainingInputForm() {
         required
       />
       <button className="training__repititions-button">+</button>
-    </div>
+    </form>
   );
 }
