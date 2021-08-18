0abaa0ce5eb9856d29c9680c6ec24cdca9c94b40
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-08-18 11:44:33 +0200

    add accordion component

--- src/pages/SingleExercise.js
+++ src/pages/SingleExercise.js
@@ -1,14 +1,8 @@
 import React from "react";
 import { useParams } from "react-router";
-import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Accordion } from "@material-ui/core";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { makeStyles } from "@material-ui/core/styles";
-import Box from "@material-ui/core/Box";
+import SingleExerciseAccordion from "../components/SingleExerciseAccordion";
 
 import "./SingleExercise.css";
 
@@ -24,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
 export default function SingleExercise() {
   const classes = useStyles();
   const [exerciseData, setExerciseData] = useState("");
-
+  const [isActive, setIsActive] = useState("false");
   const { exerciseId } = useParams();
 
   useEffect(() => {
@@ -35,6 +29,10 @@ export default function SingleExercise() {
         setExerciseData(data);
       });
   }, [exerciseId]);
+
+  const handleToggle = () => {
+    setIsActive(!isActive);
+  };
 
   return (
     <div className="singleExercise__wrapper">
@@ -49,56 +47,10 @@ export default function SingleExercise() {
           src={exerciseData?.images ? exerciseData?.images[1]?.image : null}
         />
       </div>
-      <div className="singleExercise__accordion">
-        <Box display="flex" justifyContent="center" alignItems="center">
-          <Accordion className={classes.root}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography className={classes.heading}>Description</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                {/* <div className="singleExercise__description"> */}
-                {exerciseData &&
-                  exerciseData.description.replace(/(<([^>]+)>)/gi, "")}
-                {/* </div> */}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </Box>
-
-        <Box display="flex" justifyContent="center" alignItems="center">
-          <Accordion className={classes.root}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography className={classes.heading}>Variations</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography className={classes.heading}>
-                <div className="singleExercise__variations">
-                  {exerciseData &&
-                    exerciseData.variations.map((exercise, index) => {
-                      return (
-                        <li className="singleExercise__li" key={index}>
-                          <Link className="Link" to={`/training/${exercise}`}>
-                            {exercise}
-                          </Link>
-                        </li>
-                      );
-                    })}
-                </div>
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </Box>
-        <button className="singleExercise__button-train">train exercise</button>
-      </div>
+      <SingleExerciseAccordion classes={classes} exerciseData={exerciseData} />
+      <button onClick={handleToggle} className="singleExercise__button-train">
+        train exercise
+      </button>
     </div>
   );
 }
