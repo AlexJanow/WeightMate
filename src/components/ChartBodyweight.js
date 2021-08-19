f6557d3bdefa41b0df6bf97281a2fcef5894d8bb
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-08-19 17:28:13 +0200

    fix chart

--- src/components/ChartBodyweight.js
+++ src/components/ChartBodyweight.js
@@ -2,12 +2,16 @@ import { Line } from "react-chartjs-2";
 import "./ChartBodyweight.css";
 
 export default function ChartBodyweight() {
-  const items = { ...localStorage };
+  // const items = { ...localStorage };
 
-  const dates = Object.entries(items).map(([key, value]) => key);
-  const weights = Object.entries(items).map(([key, value]) =>
-    JSON.parse(value)
-  );
+  // const dates = Object.entries(items).map(([key, value]) => key);
+  // const weights = Object.entries(items).map(([key, value]) =>
+  //   JSON.parse(value)
+  // );
+
+  const dataSet = JSON.parse(localStorage.getItem("bodyweightDataArray"));
+  const dates = dataSet.map((d) => d.date);
+  const weights = dataSet.map((w) => w.weight);
 
   const labels = dates;
   const data = {
@@ -18,8 +22,8 @@ export default function ChartBodyweight() {
         data: weights,
         fill: {
           target: "origin",
-          above: "#10AC84", // Area will be red above the origin
-          below: "red", // And blue below the origin
+          above: "#10AC84",
+          below: "red",
         },
         borderColor: "#fff",
         borderWidth: 2,
