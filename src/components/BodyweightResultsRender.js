78da4c01518393b32e2f419bb08ff37215ea8aad
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-09-05 01:22:18 +0200

    add visual rdisplay at bodyweight results render

--- src/components/BodyweightResultsRender.js
+++ src/components/BodyweightResultsRender.js
@@ -1,7 +1,7 @@
 import "./BodyweightResultsRender.css";
 import { getItemsFromLocalStorage } from "../utils/itemStorage";
 import { useEffect, useState } from "react";
-
+import { ImArrowUpRight2, ImArrowDownRight2 } from "react-icons/im";
 export default function BodyweightResultsRender({ bodyweightDataArray }) {
   const [bwLogbook, setBwLogbook] = useState([]);
   useEffect(() => {
@@ -13,6 +13,22 @@ export default function BodyweightResultsRender({ bodyweightDataArray }) {
       return (
         <li key={data.id} className="bodyweight__results-render-li">
           {data.date} - {data.weight} kg
+          {data.id === bwLogbook[bwLogbook.length - 1].id ? (
+            bwLogbook[bwLogbook.length - 2].weight < data.weight ? (
+              <>
+                {" "}
+                <ImArrowUpRight2 /> +
+                {data.weight - bwLogbook[bwLogbook.length - 2].weight} kg
+              </>
+            ) : (
+              <>
+                {" "}
+                <ImArrowDownRight2 /> -
+                {bwLogbook[bwLogbook.length - 2].weight - data.weight} kg
+              </>
+            )
+          ) : null}
+          {/* {bwLogbook} */}
         </li>
       );
     });
